6e83d958e295127c993ccd6098ca02ad695fb05b
YoungBear <youngbear@aliyun.com>
2023-02-20 22:46:05 +0800

    [init] init from spring initializr

---
+++ src/test/java/com/example/springbootjar/SpringBootJarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootjar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
